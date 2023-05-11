2d7fe10f70f594fa008050212c8d946b021de683
geovanne <geovamelo4431@gmail.com>
2023-05-10 21:11:45 -0300

    feat: base pronta

---
+++ main.js
@@ -0,0 +1,13 @@
+let modalTrue = document.getElementById('modalTrue')
+
+let modalFalse = document.getElementById('modalFalse')
+
+
+const openModalTrue = () => {
+    modalTrue.style.display = "flex"
+}
+
+
+const openModalFalse = () => {
+    modalFalse.style.display = "flex"
+}
